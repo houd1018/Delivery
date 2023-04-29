fdc5e976e8c8cd659f06a643bdf5cc3a24ad3c93
SapphireStar <1910140214@qq.com>
2023-04-29 21:56:31 +0800

    Merge UI System and InfiniteScroll

---
+++ Assets/Scripts/Manager/Game.cs
@@ -0,0 +1,59 @@
+using Cysharp.Threading.Tasks;
+using Isekai.UI.ViewModels.Screens;
+using MyPackage;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Isekai.Managers
+{
+    public class PauseGameEvent : IEventHandler
+    {
+
+    }
+    public class Game : MonoSingleton<Game>
+    {
+        public bool GameStarted;
+        async void Start()
+        {
+            await InitializeManagers();
+
+            MainMenuViewModel viewmodel = new MainMenuViewModel();
+            ScreenManager.Instance.TransitionToInstant(UI.EScreenType.MainMenuScreen,ELayerType.DefaultLayer, viewmodel);
+
+            SoundManager.Instance.PlayMusic(SoundDefine.Music_Login);
+        }
+
+        public async UniTask InitializeManagers()
+        {
+            LayerManager.Instance.Initialize();
+            ScreenManager.Instance.Initialize();
+            SoundManager.Instance.Initialize();
+            //LevelManager.Instance.Initialize();
+        }
+        public async UniTaskVoid BackToMainMenu()
+        {
+            //await LevelManager.Instance.TransitionToScene("MainMenu",null);
+            ScreenManager.Instance.TransitionToInstant(UI.EScreenType.MainMenuScreen, ELayerType.DefaultLayer, new MainMenuViewModel());
+        }
+        public void GoToPlayScene()
+        {
+            //LevelManager.Instance.TransitionToScene("PlayScene",
+            //    () => GameStarted = true).Forget();
+        }
+        public void PauseGame()
+        {
+            GameStarted = false;
+        }
+        public void ResumeGame()
+        {
+            GameStarted = true;
+        }
+        private void Update()
+        {
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Manager/ScreenManager.cs
@@ -0,0 +1,185 @@
+﻿using Cysharp.Threading.Tasks;
+using Isekai.UI;
+using Isekai.UI.ViewModels;
+using Isekai.UI.ViewModels.Screens;
+using Isekai.UI.Views;
+using Isekai.UI.Views.Screens;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Isekai.Managers
+{
+    public class ScreenManager : MonoSingleton<ScreenManager>
+    {
+        public TransitionScreen CurTransitionScreen;
+
+        private List<GameObject> m_screens = new List<GameObject>();
+        private Stack<IScreen> m_prevScreens = new Stack<IScreen>();
+        private IScreen m_curScreen;
+        
+        private void Start()
+        {
+
+        }
+        private void Update()
+        {
+
+        }
+        public void Initialize()
+        {
+
+        }
+        public void BackToPrevScreen()
+        {
+            BackToPrevScreenAsync().Forget();
+        }
+        public async UniTaskVoid BackToPrevScreenAsync()
+        {
+            LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, true);
+
+            await CurTransitionScreen.TransitionEnter();
+
+            if (m_prevScreens.Count == 0)
+            {
+                Debug.LogError("There is no prev screens");
+                return;
+            }
+            if (m_curScreen != null)
+            {
+                m_curScreen.OnExitScreen();
+                m_curScreen.Pop();
+            }
+            m_curScreen = m_prevScreens.Pop();
+            m_curScreen.Show();
+            
+            await CurTransitionScreen.TransitionOut();
+
+            m_curScreen.OnEnterScreen();
+            LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, false);
+        }
+
+        public void TransitionToInstant<TViewModel>(EScreenType screenType, ELayerType layer, TViewModel viewModel) where TViewModel:IViewModel
+        {
+            if (m_curScreen != null)
+            {
+                if (m_curScreen.CanTraceBack)
+                {
+                    m_prevScreens.Push(m_curScreen);
+                }
+                m_curScreen.OnExitScreen();
+                m_curScreen.Pop();
+                
+            }
+
+            var newScreen =Resources.Load<GameObject>(screenType.ToString()).GetComponent<Screen<TViewModel>>();
+            newScreen.AssignViewModel(viewModel);
+            newScreen.OnEnterScreen();
+            m_screens.Add(newScreen.gameObject);
+
+            m_curScreen = newScreen;
+        }
+        public async UniTask TransitionTo<TViewModel>(EScreenType screenType, ELayerType layer, TViewModel viewModel) where TViewModel : IViewModel
+        {
+            LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, true);
+
+            await CurTransitionScreen.TransitionEnter();
+
+            if (m_curScreen != null)
+            {
+                if (m_curScreen.CanTraceBack)
+                {
+                    m_prevScreens.Push(m_curScreen);
+                }
+                m_curScreen.OnExitScreen();
+                m_curScreen.Pop();
+            }
+
+            var newScreen = Resources.Load<GameObject>(screenType.ToString()).GetComponent<Screen<TViewModel>>();
+            newScreen.AssignViewModel(viewModel);
+
+            await CurTransitionScreen.TransitionOut();
+
+            newScreen.OnEnterScreen();
+
+            m_curScreen = newScreen;
+
+            m_screens.Add(newScreen.gameObject);
+
+            LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, false);
+        }
+
+        //public async UniTask TransitionToInstant(EScreenType screenType, ELayerType layer) 
+        //{
+        //    if (m_curScreen != null)
+        //    {
+        //        if (m_curScreen.CanTraceBack)
+        //        {
+        //            m_prevScreens.Push(m_curScreen);
+        //        }
+        //        m_curScreen.OnExitScreen();
+        //        m_curScreen.Pop();
+        //    }
+
+
+        //    var newScreen = await m_provider.GetScreen(screenType, layer) as IScreen;
+        //    newScreen.OnEnterScreen();
+        //    m_curScreen = newScreen;
+
+        //}
+        //public async UniTask TransitionTo(EScreenType screenType, ELayerType layer)
+        //{
+        //    LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, true);
+
+        //    await CurTransitionScreen.TransitionEnter();
+
+        //    if (m_curScreen != null)
+        //    {
+        //        if (m_curScreen.CanTraceBack)
+        //        {
+        //            m_prevScreens.Push(m_curScreen);
+        //        }
+        //        m_curScreen.OnExitScreen();
+        //        m_curScreen.Pop();
+        //    }
+
+        //    var newScreen = await m_provider.GetScreen(screenType, layer) as IScreen;
+        //    newScreen.OnEnterScreen();
+                
+        //    m_curScreen = newScreen;
+
+        //    LayerManager.Instance.SetLayerInteractable(ELayerType.TransitionLayer, false);
+        //}
+
+        public void PopAllScreenInstant()
+        {
+            m_curScreen?.OnExitScreen();
+            m_curScreen?.Pop();
+            m_curScreen = null;
+
+            while (m_prevScreens.Count > 0)
+            {
+                IScreen screen = m_prevScreens.Pop();
+                screen.OnExitScreen();
+                screen.Pop();
+                
+            }
+            foreach (var item in m_screens)
+            {
+                if (!item.Equals(null))
+                {
+                    DestroyImmediate(item);
+                }
+            }
+            m_screens.Clear();
+        }
+
+
+        public View<TViewModel> GetScreen<TViewModel>(EScreenType screenType, ELayerType layer = ELayerType.DefaultLayer) where TViewModel : IViewModel
+        {
+            var screen = Resources.Load<GameObject>(screenType.ToString()).GetComponent<Screen<TViewModel>>();
+            return screen as View<TViewModel>;
+        }
+    }
+}
+
