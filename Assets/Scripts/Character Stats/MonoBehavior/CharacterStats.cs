4d05d4608fd9f01d8c51db102dfb8797e5c26fba
Howard Hou18 <1413763509@qq.com>
2023-04-29 20:23:39 +0800

    Spawn block

---
+++ Assets/Scripts/Character Stats/MonoBehavior/CharacterStats.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public CharacterStats_SO characterData;
+    
+    #region Read from Data_SO
+    public int MaxHealth
+    {
+        get
+        {
+            if (characterData != null)
+            {
+                return characterData.maxHealth;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+        {
+            characterData.maxHealth = value;
+        }
+    }
+    public int CurrentHealth
+    {
+        get
+        {
+            if (characterData != null)
+            {
+                return characterData.currentHealth;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        set
+        {
+            characterData.currentHealth = value;
+        }
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/Character Stats/Scriptable Objects/CharacterStats_SO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Data", menuName = "Character Stats/Data")]
+public class CharacterStats_SO : ScriptableObject
+{
+    [Header("Stats Info")]
+    public int maxHealth;
+    public int currentHealth;
+}
