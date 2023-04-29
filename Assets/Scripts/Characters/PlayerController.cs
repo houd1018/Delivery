73bbe36380a5bc77858f17fe1f4b678096bd7411
Howard Hou18 <1413763509@qq.com>
2023-04-30 00:40:50 +0800

    update player control

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -4,41 +4,64 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerStates { WALK, DEAD }
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float runSpeed = 10f;
     [SerializeField] private float jumpForce = 5f;
-    [SerializeField] float paddingLeft;
-    [SerializeField] float paddingRight;
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
     private Vector2 maxBounds;
+    private PlayerStates playerStates;
 
     CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeetCollider;
+    Animator myAnimator;
 
+    // -------- anim bool----------
+    bool isWalk;
+    bool isDead;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         myBodyCollider = GetComponent<CapsuleCollider2D>();
         myFeetCollider = GetComponent<BoxCollider2D>();
+        myAnimator = GetComponentInChildren<Animator>();
+    }
 
-    }
-    private void Start()
-    {
-        initBounds();
-    }
     private void FixedUpdate()
     {
-        RestrictMove();
         Run();
+        SwitchAnimation();
+        SwitchStates();
+    }
+
+    void SwitchAnimation()
+    {
+        myAnimator.SetBool("Death", isDead);
+        myAnimator.SetBool("Walk", isWalk);
+    }
+
+    void SwitchStates()
+    {
+        switch (playerStates)
+        {
+            case PlayerStates.WALK:
+                // do things in the WALK
+                break;
+            case PlayerStates.DEAD:
+                // do things when player is dead
+                break;
+
+        }
     }
 
     private void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+        
     }
     private void OnJump(InputValue value)
     {
@@ -49,18 +72,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void initBounds()
-    {
-        Camera mainCamera = Camera.main;
-        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
-    }
-
     private void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rb.velocity.y);
         rb.velocity = playerVelocity;
+        isWalk = true;
     }
 
+// restrict moving in the camera scene
     private void RestrictMove()
     {
         if (Math.Abs(transform.position.x) > Math.Abs(maxBounds.x))
