c6aa6d649dd163c2861caba87f671bcd000515b7
Howard Hou18 <1413763509@qq.com>
2023-05-01 23:26:05 +0800

    upload conveyor sprite, complete sprite flip

--- Assets/Scripts/Blocks/Blocks/TestConveyBlock.cs
+++ Assets/Scripts/Blocks/Blocks/TestConveyBlock.cs
@@ -6,11 +6,17 @@ public class TestConveyBlock : BaseBlock
 {
     [SerializeField] float conveySpeed = 1f;
     int isRight; // random value for left convey or right convey
+    SpriteRenderer spriteRenderer;
 
     protected override void Start()
     {
         base.Start();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         isRight = (Random.Range(0, 2) * 2) - 1;
+        if (isRight != 1)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
     protected override void Update()
     {
